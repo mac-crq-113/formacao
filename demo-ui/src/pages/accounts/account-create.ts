7bf340171c12b35881342f58a143caa92a9eabb1
Pedro Silva <pedro.m2.silva@gmail.com>
2021-11-24 01:15:04 +0000

    Fix bug criação de conta

--- demo-ui/src/pages/accounts/account-create.ts
+++ demo-ui/src/pages/accounts/account-create.ts
@@ -6,18 +6,11 @@ import { AccountsService } from '../../services/accounts-service';
 @inject()
 export class AccountCreate {
 
-    private formElem: HTMLFormElement;
 
     private account: IAccount;
-    private password2: string;
 
     constructor(private accountsService: AccountsService, @IRouter private router: IRouter) {
         this.account = {};
-    }
-
-    get disable() {
-        console.info("test")
-        return this.account.password != this.password2 || this.formElem.checkValidity() === false;
     }
 
     criar(_evt: SubmitEvent) {
